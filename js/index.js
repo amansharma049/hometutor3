4f8451d0e3dba49098376d55b28880cccc03c5a2
Aman Sharma <98725523+amansharma049@users.noreply.github.com>
2022-11-25 12:38:48 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -26,3 +26,14 @@ if (close) {
           }
         }
       }
+
+let hour = new Date().getHours();
+let greeting;
+  if (hour < 12) {
+    greeting = "Good Morning";
+  }else if (hour < 16) {
+    greeting = "Good Afternoon";
+  } else {
+    greeting = "Good Evening";
+  }
+document.getElementById("good").innerHTML = greeting;
